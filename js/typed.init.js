2d5f0bed90f989669d00790585a5201862805cce
Javier Gonzalez <javier.gonzalez@quadrant.travel>
2026-05-04 14:12:16 +0200

    feat(i18n): expose Typed instance on window for lang re-init

--- js/typed.init.js
+++ js/typed.init.js
@@ -1,8 +1,9 @@
 
+
 // Typed Text
 var typedEl = document.querySelector(".element");
 if (typedEl) {
-    new Typed(".element", {
+    window.__typedInstance = new Typed(".element", {
         strings: typedEl.getAttribute("data-elements").split(","),
         typeSpeed: 100,
         backDelay: 3000,
